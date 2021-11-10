cc1f0d4f4bd68a44a13769526b6969ec05215bc2
vickeytt <40120796+vickeytt@users.noreply.github.com>
2021-11-10 15:45:41 +0800

    Update 进程挂起和恢复.py

--- 进程挂起和恢复.py
+++ 进程挂起和恢复.py
@@ -20,6 +20,7 @@ def Resume_thread(pid):
 if __name__ == '__main__':
     p = Process(target=fun1,args=('0',)) #实例化进程对象
     p.start()
+    p.join()#利用多线程时，一般都先让子线程调用start() ，然后再去调用join()，让主进程等待子进程结束才继续走后续的逻辑
     print(p.pid)
     while 1:
         keyboard.wait('enter')#等待键盘回车输入
